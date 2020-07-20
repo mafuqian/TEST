00c5b1c7a0feaeeac3013011469abb705d0c67a5
mafuqian <fuqian.ma@hotmail.com>
2020-07-20 18:20:49 +0800

    del

--- mbt_hh_20200717.sql
+++
@@ -1,2 +0,0 @@
-update gp_qc_rule_set set source = ' AND CUST_TYPE=''C''' where DATA_ID = 'MBT_510_1700501';
-update gp_qc_rule_set set source = ' AND CUST_TYPE=''P''' where DATA_ID = 'MBT_510_28010101';
